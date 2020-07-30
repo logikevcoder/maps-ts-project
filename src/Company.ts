b74707d18a00d6b371ed7962e21162a3ca56eea4
Kevin Napier <kevin.napier@rotary.org>
2020-07-30 15:15:31 -0500

    updated the different typescript files to accept data for marker popup information, exported the interface from map to be implemented by User and Company

--- src/Company.ts
+++ src/Company.ts
@@ -1,19 +1,28 @@
 import faker from 'faker';
+import { Mappable } from './Map';
 
-export class Company {
-  companyName: string;
+export class Company implements Mappable {
   name: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
 
   constructor() {
-    this.companyName = faker.company.companyName();
-    this.name = faker.company.catchPhrase();
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>User Name: ${this.name}</h1>
+      <h2>CatchPhrase: ${this.catchPhrase}</h2>
+    </div>`;
   }
 }
--- src/Map.ts
+++ src/Map.ts
@@ -1,8 +1,9 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class Map {
@@ -19,12 +20,20 @@ export class Map {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
