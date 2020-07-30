092a54ba71551b743dee8abd87b825a81bd71278
Kevin Napier <kevin.napier@rotary.org>
2020-07-30 14:10:59 -0500

    built the map ts file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
+import { Map } from './Map';
 import { User } from './User';
-import { Company } from './company';
+import { Company } from './Company';
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const user = new User();
+const company = new Company();
+const customMap = new Map('map');
 
-console.log('map', map);
+customMap.addMarker(user);
+customMap.addMarker(company);
