11486f351f1fcf3cd837cd86029ab4f9690e79be
Kevin Napier <kevin.napier@rotary.org>
2020-07-30 11:40:46 -0500

    added googlemaps library and type definition library, added a test import of the google map in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import { User } from './User';
+import { Company } from './company';
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
+
+console.log('map', map);
